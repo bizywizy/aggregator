b630f63ca7631c5a368a95e71238826df3dedce2
slavrov <slavrov93@gmail.com>
2017-08-29 02:07:37 +0300

    db now uses M:M relationship and added commands

--- models.py
+++ models.py
@@ -1,12 +1,18 @@
 from core import db
 
+subscriptions = db.Table('subscriptions',
+                         db.Column('subscription_id', db.Integer, db.ForeignKey('subscription.id')),
+                         db.Column('subscriber', db.Integer, db.ForeignKey('subscriber.id'))
+                         )
+
 
 class Subscription(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.VARCHAR(255))
+    name = db.Column(db.VARCHAR(255), unique=True)
     url = db.Column(db.VARCHAR(255))
     last_link = db.Column(db.VARCHAR(255))
-    subscriber_id = db.Column(db.Integer, db.ForeignKey('subscriber.id'), nullable=True)
+    subscribers = db.relationship('Subscriber', secondary=subscriptions,
+                                  backref=db.backref('subscriptions', lazy='dynamic'))
 
     def __init__(self, name, url, last_link, subscriber_id):
         self.name = name
@@ -14,16 +20,34 @@ class Subscription(db.Model):
         self.last_link = last_link
         self.subscriber_id = subscriber_id
 
+    @classmethod
+    def get_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
+
+    def __str__(self):
+        return self.name
+
 
 class Subscriber(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    chat_id = db.Column(db.VARCHAR(255))
+    chat_id = db.Column(db.VARCHAR(255), unique=True)
     username = db.Column(db.VARCHAR(255), nullable=True)
     first_name = db.Column(db.VARCHAR(255), nullable=True)
     last_name = db.Column(db.VARCHAR(255), nullable=True)
-    subscriptions = db.relationship('Subscription', backref='subscriber', lazy='dynamic')
+    subscription_id = db.Column(db.Integer, db.ForeignKey('subscription.id'), nullable=True)
 
     def __init__(self, chat_id, username=None, first_name=None, last_name=None):
         self.chat_id = chat_id
         db.session.add(self)
         db.session.commit()
+
+    @classmethod
+    def get_or_create(cls, chat_id, **kwargs):
+        subscriber = cls.query.filter_by(chat_id).first()
+        if subscriber:
+            return subscriber, False
+        else:
+            subscriber = cls(chat_id)
+            db.session.add(subscriber)
+            db.session.commit()
+            return subscriber, True
--- views.py
+++ views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 
-from models import Subscriber
+from core import db
+from models import Subscriber, Subscription as SubscriptionDomain
 from subscription import Subscription
 
 views = Blueprint('views', __name__)
@@ -22,3 +23,53 @@ def receive_update():
         subscriber = Subscriber(chat.get('id'))
         s = Subscription.subscribe(name, url, subscriber.id)
     return jsonify({})
+
+
+# commands
+def start(chat_id, **kwargs):
+    Subscriber(chat_id)
+    return {'status': 'ok', 'response': 'created subscriber'}
+
+
+def subscribe(subscriber, name):
+    subscription = SubscriptionDomain.get_by_name(name)
+    if not subscription:
+        return {'status': 'error', 'response': 'not found subscription \'{}\''.format(name)}
+    subscription.subscribers.append(subscriber)
+    db.session.add(subscription)
+    db.session.commit()
+    return {'status': 'ok', 'response': 'subscribed to \'{}\''.format(name)}
+
+
+def unsubscribe(subscriber, name):
+    subscription = SubscriptionDomain.get_by_name(name)
+    if not subscription:
+        return {'status': 'error', 'response': 'not found subscription \'{}\''.format(name)}
+    try:
+        subscription.subscribers.remove(subscriber)
+        db.session.add(subscription)
+        db.session.commit()
+    except ValueError:
+        return {'status': 'error', 'response': 'you are not subscribed to \'{}\''.format(name)}
+    return {'status': 'ok', 'response': 'unsubscribed to \'{}\''.format(name)}
+
+
+def own_subscription_list(subscriber):
+    subscriptions = subscriber.subscriptions.all()
+    if not subscriptions:
+        return {'status': 'error', 'response': 'you have no subscriptions yet'}
+    return {'status': 'ok', 'response': {'subscriptions': [str(subscription) for subscription in subscriptions]}}
+
+
+def all_subscription_list():
+    subscriptions = SubscriptionDomain.query.all()
+    if not subscriptions:
+        return {'status': 'error', 'response': 'we have no subscriptions yet'}
+    return {'status': 'ok', 'response': {'subscriptions': [str(subscription) for subscription in subscriptions]}}
+
+
+# utils
+def parse_text(text):
+    commands = ('/start', '/subscribe', '/unsubscribe', '/my_subscriptions', '/subscriptions')
+    if not text.startswith('/'):
+        return {'status': 'error', 'response': 'please use one of /commands'}
